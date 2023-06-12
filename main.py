aa1f833f485a75db6e8a73a28e24a76562ed5dc6
wannawhat <artemyi03@gmail.com>
2023-06-12 14:24:03 +0300

    Test pre-commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import logging
+import os
+
+import rich
+
+from somefile import script
 
 if __name__ == '__main__':
+    os.getenv('fd')
     print('Hello')
---
+++ somefile/script.py
@@ -0,0 +1,12 @@
+
+import logging
+from logging import (debug, getLevelName, getLogger, getLoggerClass,
+                     getLogRecordFactory, log, logMultiprocessing,
+                     logProcesses, logThreads)
+
+a = 'dsfgkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkksdfs'
+
+
+
+
+logging.getLogger()
